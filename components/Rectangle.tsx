69c7fb2daa90aba45ab5231302f098dd1da444e4
Luís Farias <luisgmfarias@gmail.com>
2022-06-16 10:52:19 -0300

    add rectangle div

---
+++ components/Rectangle.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@stitches/react";
+import { colors } from "../styles/colors";
+
+export const Rectangle = styled("div", {
+  backgroundColor: colors.primary500,
+  borderRadius: "20px",
+  width: "43px",
+  height: "11px",
+  margin: "16px 0",
+});
